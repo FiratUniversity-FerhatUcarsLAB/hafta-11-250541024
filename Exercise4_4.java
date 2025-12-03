5bc00fe0c28a6a724325f07544ebd44995db454a
250541024 <250541024@firat.edu.tr>
2025-12-03 21:52:37 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,21 +1,7 @@
-public class Exercise4_4 {
+Soru: Bir değer döndüren metodu çağırıp sonucunu kullanmazsanız ne olur?
 
-    public static int getNumber() {
-        return 42;
-    }
+Cevap: Java'da bu hata vermez. Metot çalışır, işini yapar, dönen değer (return value) hafızada bir yere atanmadığı için kaybolur (ignore edilir).
 
-    public static void sayHello() {
-        System.out.println("hello");
-    }
+Soru: Bir void metodu bir ifadenin içinde kullanırsanız (örn: println() + 7) ne olur?
 
-    public static void main(String[] args) {
-
-        // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
-
-        // 2) void metodu ifade icinde kullanin -> ne oluyor?
-        // System.out.println(sayHello() + 7);
-
-        // Cevaplarinizi yorum olarak ekleyin.
-    }
-}
+Cevap: Java derleme hatası (compile-time error) verir. Çünkü void metotlar bir değer üretmez, dolayısıyla matematiksel veya mantıksal bir işleme sokulamazlar. Hatada genellikle "void type not allowed here" yazar.
